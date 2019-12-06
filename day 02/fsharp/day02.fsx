ce0806562e62fd081ca46e3f8b98b038f3b19de1
ofraski <kofi@wimiro.com>
2019-12-06 14:36:56 +0100

    day #2 solution in F# (part two)

--- day 02/fsharp/day02.fsx
+++ day 02/fsharp/day02.fsx
@@ -1,40 +1,40 @@
-﻿type Program = int list
+﻿type Memory = int list
 
 let jump = 4
 
 let haltCode = 99
 
-let ops =
+let opCodes =
   [
     1, (+); 
     2, (*);
   ]
   |> Map.ofList
 
-let position (program: Program) index offset =
-  program.[index + offset]
+let address (memory: Memory) pointer offset =
+  memory.[pointer + offset]
 
-let valueAt (program: Program) index offset = 
-  program.[position program index offset]
+let valueAt (memory: Memory) pointer offset = 
+  memory.[address memory pointer offset]
 
-let replace value index (elements: Program) =
-  let update = fun i existing -> if i = index then value else existing
+let replace value pointer (elements: Memory) =
+  let update = fun i existing -> if i = pointer then value else existing
   elements |> List.mapi update
 
-let rec reProgram (program: Program) index =
-  let getUpdated (program: Program) index =
-    let op = ops.[program.[index]]
-    let get = valueAt program index
-    let value = op (get +1) (get +2)
-    let at = position program index +3
-    let updated = replace value at program
-    reProgram updated (index + jump)
+let rec reProgram (memory: Memory) pointer =
+  let getUpdated (memory: Memory) pointer =
+    let op = opCodes.[memory.[pointer]]
+    let parameter = valueAt memory pointer
+    let value = op (parameter +1) (parameter +2)
+    let at = address memory pointer +3
+    let updated = replace value at memory
+    reProgram updated (pointer + jump)
 
-  if program.[index] = haltCode then program
-  else getUpdated program index
+  if memory.[pointer] = haltCode then memory
+  else getUpdated memory pointer
 
-let fix (program: Program) =
-  reProgram program 0
+let fix (memory: Memory) =
+  reProgram memory 0
 
 let assertThat condition =
   if condition then "ok"
@@ -48,5 +48,22 @@ assertThat ([30;1;1;4;2;5;6;0;99] = fix [1;1;1;4;99;5;6;0;99])
 
 let input = [1;12;2;3;1;1;2;3;1;3;4;3;1;5;0;3;2;10;1;19;2;19;6;23;2;13;23;27;1;9;27;31;2;31;9;35;1;6;35;39;2;10;39;43;1;5;43;47;1;5;47;51;2;51;6;55;2;10;55;59;1;59;9;63;2;13;63;67;1;10;67;71;1;71;5;75;1;75;6;79;1;10;79;83;1;5;83;87;1;5;87;91;2;91;6;95;2;6;95;99;2;10;99;103;1;103;5;107;1;2;107;111;1;6;111;0;99;2;14;0;0]
 
-let partOne = (fix input).[0]
+let partOne input = (fix input).[0]
 
+partOne input // <- part one's answer is here
+
+let partTwo input seeking =
+  seq {
+    for noun = 0 to 99 do
+      for verb = 0 to 99 do
+        let replacedNoun = replace noun +1 input
+        let replacedBoth = replace verb +2 replacedNoun
+        let result = fix replacedBoth
+        yield noun, verb, result.[0] = seeking  
+  }
+
+let producesExpectedOutput (_, _, found) = found
+
+partTwo input 19690720  // <- part two's answer is here
+|> Seq.filter producesExpectedOutput
+|> Seq.map (fun (noun, verb, _) -> noun * 100 + verb)
