3860154c8c09b96fb8474737d4643157f5e5b103
ofraski <kofi@wimiro.com>
2019-12-09 23:45:34 +0100

    day #5 solution in F# (part two)

--- day 05/fsharp/day05.fsx
+++ day 05/fsharp/day05.fsx
@@ -9,6 +9,10 @@ type Opcode =
   | Multiply = 2
   | Input = 3
   | Output = 4
+  | JumpTrue = 5
+  | JumpFalse = 6
+  | LessThan = 7
+  | Equals = 8
   | Halt = 99
 
 type Mode =
@@ -48,22 +52,25 @@ let display pointer value =
 
 let diagnose input (memory: Memory) pointer =
   let i = parseInstruction (string <| memory.[pointer])
-  let from index (param:Parameter) = valueByMode param.[index] memory (pointer + index)
+  let arg index (param:Parameter) = valueByMode param.[index] memory (pointer + index)
   let at offset = memory.[pointer + offset] 
-  let perform op = op (from 1 i.Parameter) (from 2 i.Parameter)
+  let perform op = op (arg 1 i.Parameter) (arg 2 i.Parameter)
   let update op = (replace memory memory.[pointer + 3] (perform op))
 
   match (i.Opcode) with
   | Opcode.Add -> (update (+), pointer + 4)
   | Opcode.Multiply -> (update (*), pointer + 4)
   | Opcode.Input -> ((replace memory memory.[pointer + 1] input), pointer + 2)
-  | Opcode.Output -> display pointer (from 1 i.Parameter); (memory, pointer + 2)
+  | Opcode.Output -> display pointer (arg 1 i.Parameter); (memory, pointer + 2)
+  | Opcode.JumpTrue -> if (arg 1 i.Parameter) <> 0 then (memory, (arg 2 i.Parameter)) else (memory, pointer + 3)
+  | Opcode.JumpFalse -> if (arg 1 i.Parameter) = 0 then (memory, (arg 2 i.Parameter)) else (memory, pointer + 3)
+  | Opcode.LessThan -> if (arg 1 i.Parameter) < (arg 2 i.Parameter) then ((replace memory memory.[pointer + 3] 1), pointer + 4) else ((replace memory memory.[pointer + 3] 0), pointer + 4)
+  | Opcode.Equals -> if (arg 1 i.Parameter) = (arg 2 i.Parameter) then ((replace memory memory.[pointer + 3] 1), pointer + 4) else ((replace memory memory.[pointer + 3] 0), pointer + 4)
   | Opcode.Halt -> (memory, pointer)
   | _ -> invalidArg "i.Opcode" (sprintf "Unrecognised opcode %A" i.Opcode)
 
 let rec runDiagnostic input (memory: Memory) pointer =
     let reprogrammed, next = diagnose input memory pointer
-
     if reprogrammed.[next] = 99 then true
     else runDiagnostic input reprogrammed next
 
@@ -72,4 +79,8 @@ let memory =
   |> fun x -> x.Split(',')
   |> Array.map int
 
+// Part One
 // runDiagnostic 1 memory 0
+
+// Part Two
+// runDiagnostic 5 memory 0
