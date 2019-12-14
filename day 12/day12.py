9ba73e72a0de55f9ce5d523661640962f4cdab5e
ofraski <kofi@wimiro.com>
2019-12-14 16:52:45 +0000

    day #12 solution in Python (part two)

--- day 12/day12.py
+++ day 12/day12.py
@@ -1,8 +1,17 @@
+part_one = 0
+part_two = 0
+
 pos = []
 pos.append({"x":-16, "y":15, "z":-9})
 pos.append({"x":-14, "y":5, "z":4})
 pos.append({"x":2, "y":0, "z":6})
 pos.append({"x":-3, "y":18, "z":9})
+
+inital = []
+inital.append({"x":-16, "y":15, "z":-9})
+inital.append({"x":-14, "y":5, "z":4})
+inital.append({"x":2, "y":0, "z":6})
+inital.append({"x":-3, "y":18, "z":9})
 
 vel = []
 vel.append({"x":0, "y":0, "z":0})
@@ -25,7 +34,19 @@ def update_vels(old_vels, new_vels, axis):
         old_vels[i][axis] += new_vels[i]
     return old_vels
 
-for times in range(0, 1000):
+def greatest_common_divisor(fst, snd):
+    while snd > 0:
+        fst, snd = snd, fst % snd
+    return fst
+
+def lowest_common_multiple(fst, snd):
+    return fst * snd / greatest_common_divisor(fst, snd)
+
+found = {"x": 0, "y": 0, "z": 0}
+
+time = 0
+
+while True:
     for axis in axes:
         vels = get_vels(axis, pos)
         update_vels(vel, vels, axis)
@@ -34,7 +55,27 @@ for times in range(0, 1000):
         for i in range(len(pos)):
             pos[i][axis] += vel[i][axis]
 
-energy = 0
+    time += 1
 
-for i in range(len(pos)):
-    energy += sum([abs(x) for x in pos[i].values()]) * sum([abs(x) for x in vel[i].values()])
+    if time == 1000:
+        for i in range(len(pos)):
+            part_one += sum([abs(x) for x in pos[i].values()]) * sum([abs(x) for x in vel[i].values()])
+
+    for axis in axes:
+        if found[axis] != 0:
+            continue
+
+        for i in range(len(pos)):
+            if vel[i][axis] != 0:
+                break
+
+            if pos[i][axis] != inital[i][axis]:
+                break
+        
+        else:    
+            found[axis] = time
+
+    if found["x"] != 0 and found["y"] != 0 and found["z"] != 0:
+        lcm = lowest_common_multiple
+        part_two = lcm(lcm(found["x"], found["y"]),found["z"])
+        break
